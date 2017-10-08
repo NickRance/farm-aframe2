1e72a65280e21471b01e8e63d132ae6d8e847ed8
Gizzada <christopher.john.gayle@gmail.com>
2017-10-08 17:56:40 -0400

    556

--- meteor/client/main.js
+++ meteor/client/main.js
@@ -10,11 +10,14 @@
   8a. the farmer is instructed to harvest plant, which earns user coins
   8b. too much time passes and the plant rots
 */
+aframe = require('aframe');
 
-Session.set('coins',0); aframe = require('aframe');
-// require('aframe-slideshow-component');
+Session.set('coins',0); Session.set('second',0); Session.set('minute',0); 
+Session.set('house','house'); Session.set('animation',false);
+Session.set('tree',[]);
 
 Template.client.onRendered(function () {
+  gameclock();
   var instance = this;
 
   instance.autorun(function () {
@@ -22,17 +25,25 @@ Template.client.onRendered(function () {
       aframe.registerComponent('cursor-listener', {
         init: function () {
           var lastIndex = -1;
+
           this.el.addEventListener('click', function (evt) {
             if(evt.target.id.search('tree')){
-              document.getElementById('tree'+evt.target.id).setAttribute('src', '#tree');
+              snoopmove(evt.target.id);
             } else {
               document.getElementById(evt.target.id).setAttribute('src', null);
               var coins = Session.get('coins')+3; Session.set('coins',coins);
             }
           });
-          this.el.addEventListener('mouseenter', function (evt) { });
+          this.el.addEventListener('mouseenter', function (evt) {
+            document.getElementById(evt.target.id).setAttribute('opacity', '0.4');
+            document.getElementById(evt.target.id).setAttribute('color', 'green');
+          });
 
-          this.el.addEventListener('mouseexit', function (evt) { });
+          this.el.addEventListener('mouseleave', function (evt) {
+            document.getElementById(evt.target.id).setAttribute('opacity', '1');
+            document.getElementById(evt.target.id).setAttribute('color', null);
+          });
+
         }
       });
     }
@@ -46,16 +57,24 @@ Template.scene.helpers({
     while(z <= count){
       while(x <= count){
         x+=increment;
-        var plot = { x: x, z: z, i: increment, x2: x-(increment/2), z2: z+(increment/2) };
+        var plot = { x: x-5, z: -z, i: increment, x2: x-(increment/2)-5, z2: -z+(increment/2) };
         list.push(plot);
       } z+=increment; x = 0;
     }
 
     return list;
   },
+  animation : function () {
+    if(Session.get('animation')){ return Session.get('animation'); }
+    else { return null; }
+  },
 }); 
 
 Template.gui.helpers({
+  time : function () {
+    if(Session.get('minute')) { return Session.get('minute') + ':' + Session.get('second'); }
+    else { return Session.get('second'); }
+  },
   coins : function () {
     return Session.get('coins');
   },
@@ -87,3 +106,36 @@ Template.gui.events({
     Session.set('selectdigsoil',false); Session.set('selectplant',false); Session.set('selectharvest',true);
   },
 }); 
+
+function gameclock() {
+  var second = Session.get('second');
+  var minute = Session.get('minute');
+  second = second+1; 
+  if(second > 59){ second = 0; minute = minute+1; }
+  Session.set('second',second); 
+  Session.set('minute',minute); 
+  Meteor.setTimeout(function(){ gameclock(); },1000);
+}
+
+function snoopmove(landid) {
+  
+  var snoop = document.getElementById('snoop');
+  var land = document.getElementById(landid);
+  
+  var landx = landid.substring(0,land.id.search(','));
+  var landz = landid.substring(land.id.search(',')+1);
+
+  var animation = {
+    property: 'position',
+    duration: 4000,
+    from: snoop.object3D.position.x + ' ' + snoop.object3D.position.y + ' ' + snoop.object3D.position.z,
+    to: landx + ' ' + land.object3D.position.y + ' ' + landz,
+  }
+
+  Session.set('animation',animation);
+
+  Meteor.setTimeout(function(){ 
+    document.getElementById('tree'+landid).setAttribute('src', '#tree');
+    Session.set('animation',null); 
+  },animation.duration);
+}
